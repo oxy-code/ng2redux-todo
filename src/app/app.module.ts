a59bcb8a7c2393c6cc8b9e3f363ed0bff5d6f29e
Velu <velmurugan@proisc.com>
2017-05-25 21:23:23 +0530

    Design implementation 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoFooterComponent
   ],
   imports: [
     BrowserModule,
